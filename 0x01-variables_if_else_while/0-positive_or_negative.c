80fec481cd4e684c69f397532b94c86fac8234b1
Ebenezer Ofoe <ofoett@gmail.com>
2022-03-11 13:17:18 -0800

    dd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,7 @@ if (n > 0)
 printf("%i is positive\n", n);
 else if (n < 0)
 printf("%i is negative\n", n);
-else									
-printf("%i is zero\n", n);
+else										printf("%i is zero\n", n);
 
 return (0);
 }
