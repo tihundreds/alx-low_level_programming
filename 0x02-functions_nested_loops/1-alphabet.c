276e8c47a132b8a9a1e76177edbae7e96e8ef474
Ebenezer Ofoe <ofoett@gmail.com>
2022-03-15 15:50:47 -0700

    submit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,1 @@
-nclude "main.h"
 
-/**
-* print_alphabet - print alphabet in lowercase.
-*/
-void print_alphabet(void)
-{
-char ch;
-ch = 'a';
-while (ch <= 'z')
-{
-_putchar(ch);
-ch++;
-}
-_putchar('\n');
-}
