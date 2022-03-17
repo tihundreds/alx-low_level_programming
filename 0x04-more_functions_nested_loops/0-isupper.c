b5bfacf62d5456c9fddd05e97886f24d410541ff
Ebenezer Ofoe <ofoett@gmail.com>
2022-03-17 15:04:56 -0700

    fixerror

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
-* _isupper - checks for uppercase character
-* @c: the character to be checked
-* Return: 1 if c is uppercase, 0 otherwise
+* _isupper -  checks for uppercase character.
+* @c: c -  Variable
+* Return: Always 0.
 */
 int _isupper(int c)
 {
-return (c >= 'A' && c <= 'Z');
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,16 @@
-#ifndef FILE_main.h
-#define FILE_main.h
+#ifndef FILE_MAIN.H
+#define FILE_MAIN.H
 
-int _putchar(char c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month,int day);
+void print_remaining_days(int month,int day,int year);
+void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
