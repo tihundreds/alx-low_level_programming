5086adb550c36421a11567c1c38f9936c33d8607
Ebenezer Ofoe <ofoett@gmail.com>
2022-04-03 15:18:15 -0700

    real

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* main - multiplies two numbers and prints the result
-* @argc: argument count must be exactly two numbers
-* @argv: argument vector
-* Return: 0 on success, 1 on failure
+* main - multiplies two numbers
+* @argc: n args
+* @argv: arr args
+* Return: 0
 */
 int main(int argc, char *argv[])
 {
-int res1, res2;
+int i, val = 1;
 
-res1 = res2 = 0;
-if (argc == 3)
+if (argc != 3)
 {
-res1 = atoi(argv[1]);
-res2 = atoi(argv[2]);
-printf("%d\n", res1 * res2);
-return (0);
-}
-else
 printf("Error\n");
 return (1);
+}
+for (i = 1; i < argc; i++)
+{
+val *= atoi(argv[i]);
+}
+printf("%d\n", val);
+return (0);
 }
