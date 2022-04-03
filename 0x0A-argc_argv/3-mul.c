212e66ae56b3602e6649d4af78ec89cf2b958d1b
Ebenezer Ofoe <ofoett@gmail.com>
2022-04-03 14:47:19 -0700

    corrected

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,11 @@
 int main(int argc, char *argv[])
 {
 int res1, res2;
+
 res1 = res2 = 0;
 if (argc == 3)
 {
 res1 = atoi(argv[1]);
-
 res2 = atoi(argv[2]);
 printf("%d\n", res1 * res2);
 return (0);
