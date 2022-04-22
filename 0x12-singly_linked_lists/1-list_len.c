c23b50eb9ff774b425f8497a87281915c1257ff3
Ebenezer Ofoe <ofoett@gmail.com>
2022-04-21 19:21:07 -0700

    printlinked

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,13 @@
  **/
 size_t list_len(const list_t *h)
 {
-		size_t cont = 0;
+	size_t cont = 0;
 
-		while (h)
-		{
-cont++;
-h = h->next;
-		}
+	while (h)
+	{
+            cont++;
+            h = h->next;
+	}
 	
-		return (cont);
+	return (cont);
 }
