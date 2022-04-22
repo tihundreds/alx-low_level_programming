b5a1dda0d195afa0773a9a1ae9e2e680de1c78a4
Ebenezer Ofoe <ofoett@gmail.com>
2022-04-22 00:13:14 +0000

    printlist linked

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#ifndef LISTS
-#define LISTS
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -25,4 +26,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
