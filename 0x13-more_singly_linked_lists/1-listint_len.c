969d16c3bac3a4d5cb9c640bca12c86feea39592
Ebenezer Ofoe <ofoett@gmail.com>
2022-04-26 20:10:15 +0000

    1-listint

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * listint_len - function that returns the number of elements
- * in a linked listint_t list.
- *
- * @h: Parameter Pointer to the head of the linked list
- *
- * Return: The number of elements
- */
+* listint_len - function that returns the number of elements
+* in a linked listint_t list.
+*
+* @h: Parameter Pointer to the head of the linked list
+*
+* Return: The number of elements
+*/
 size_t listint_len(const listint_t *h)
 {
 	size_t n = 0;
